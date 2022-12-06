0a27308f304171dd4564b8d980909469f00b648a
Igor Melnichenko <imelnicenko498@gmail.com>
2022-12-06 16:42:03 +0300

    Check smalles values

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -2,13 +2,20 @@ package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> result = new HashMap<>();
         for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
-            result.put(entry.getValue(),entry.getKey());
+            if (result.containsKey(entry.getValue())) {
+                if (result.get(entry.getValue()) > entry.getKey()) {
+                    result.put(entry.getValue(), entry.getKey());
+                }
+            }
+            else{
+                result.put(entry.getValue(), entry.getKey());
+            }
         }
         return result;
     }
